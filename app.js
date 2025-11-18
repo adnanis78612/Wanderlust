34902920bb7ae377fc73c65ea3df250057cdc4af
adnanis78612 <adnanis78612@gmail.com>
2025-11-18 15:47:27 +0530

    new https

--- app.js
+++ app.js
@@ -63,10 +63,10 @@ const sessionOptions={
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-}
+  },
 };
 
 if (process.env.NODE_ENV === "production") {
