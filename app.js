c9190675542c85c2abffb9f14400b046530ff690
adnanis78612 <adnanis78612@gmail.com>
2025-11-18 16:20:03 +0530

    new https10

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const store=MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: {
+  cryto: {
     secret: process.env.SECRET,
   },
   touchAfter: 24*3600,
